e150fb50def5253aa2419f99e046454b94cb2fcc
Vincent Leung <vincent.leung@crypto.com>
2019-12-20 12:53:20 +0800

    Initial commit

---
+++ src/test/java/com/crypto/artifactorydemo/ArtifactoryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.artifactorydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactoryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
